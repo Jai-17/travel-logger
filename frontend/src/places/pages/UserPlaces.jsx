7b7d5f97b95f11615c7e70b2a6f343971544de63
Jai-17 <madhukarjai9@gmail.com>
2024-06-23 20:00:31 +0530

    minor fix

--- frontend/src/places/pages/UserPlaces.jsx
+++ frontend/src/places/pages/UserPlaces.jsx
@@ -5,7 +5,7 @@ const DUMMY_PLACES = [
         id: 'p1',
         title: 'Empire State Building',
         description: 'One of the most famous sky scrapers in the world!.',
-        imageUrl: '',
+        imageUrl: 'https://media.timeout.com/images/101705309/image.jpg',
         address: 'WOW',
         location: {
             lat: 40,
@@ -17,7 +17,7 @@ const DUMMY_PLACES = [
         id: 'p2',
         title: 'Empire States Building',
         description: 'One ofd the most famous sky scrapers in the world!.',
-        imageUrl: '',
+        imageUrl: 'https://www.esbnyc.com/sites/default/files/2020-01/ESB%20Day.jpg',
         address: 'WOW',
         location: {
             lat: 40,
