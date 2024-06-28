443033375c8cff5ab1c46466ab7d61ba4e84f19e
Jai-17 <madhukarjai9@gmail.com>
2024-06-28 23:52:49 +0530

    added form controls

--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,3 +1,17 @@
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators.js'
+import "./NewPlace.css";
+
 export default function NewPlacePage() {
-    return <h2>NewPlace Works!</h2>
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        label="Title"
+        type="text"
+        validators={[VALIDATOR_REQUIRE]}
+        errorText="Please enter a valid title"
+      />
+    </form>
+  );
 }
