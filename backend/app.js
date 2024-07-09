a9f0afb85a1451babe2748ad6f67644d88eb4240
Jai-17 <madhukarjai9@gmail.com>
2024-07-09 19:18:53 +0530

    user routes

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,13 @@ const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
 const HttpError = require('./controllers/places-controllers');
-
+const userRoutes = require('./routes/users-routes');
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes); // => /api/places...
+app.use('/api/users', userRoutes);
 
 app.use((req, res, next) => {
     const error = new HttpError('Could not find this route.', 404);
