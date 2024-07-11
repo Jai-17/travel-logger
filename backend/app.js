de143fe9bd4e2a47758212a7a851e785d295638d
Jai-17 <madhukarjai9@gmail.com>
2024-07-11 08:58:46 +0530

    backend initiated

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
-const HttpError = require('./controllers/places-controllers');
-const userRoutes = require('./routes/users-routes');
+const HttpError = require("./controllers/places-controllers");
+const userRoutes = require("./routes/users-routes");
 const app = express();
+
+require('dotenv').config();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes); // => /api/places...
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 app.use((req, res, next) => {
-    const error = new HttpError('Could not find this route.', 404);
-    throw error;
-})
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
@@ -24,4 +27,14 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.uxfclu9.mongodb.net/places?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    console.log("Connected!");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -3,6 +3,7 @@ const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const getRandomCoordinates = require("../util/location");
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
@@ -18,38 +19,60 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
 
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not find a place.",
+      500
+    );
+    return next(error);
   }
 
-  res.json({ place });
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided id.", 404)
+    );
+  }
+
+  res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlaceByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.filter((p) => {
-    return p.creator === userId;
-  });
 
-  if (!place) {
+  let places;
+  try {
+    places = await Place.find({ creator: userId });
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching places failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!places || places.length === 0) {
     return next(
       new HttpError("Could not find a place for the provided user id.", 404)
     );
   }
 
-  res.json({ place });
+  res.json({
+    places: places.map((place) => place.toObject({ getters: true })),
+  });
 };
 
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(HttpError("Invalid inputs passed, please check your data.", 422));
+    return next(
+      HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
 
   const { title, description, address, creator } = req.body;
@@ -61,16 +84,25 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-  const createdPlace = {
-    id: uuid(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image:
+      "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg",
     creator,
-  };
+  });
 
-  DUMMY_PLACES.push(createdPlace);
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Creating place failed, please try again.",
+      500
+    );
+    return next(error);
+  }
 
   res.status(201).json({ place: createdPlace });
 };
